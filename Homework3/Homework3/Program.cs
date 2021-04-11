9e7b95271266abd6874b25f92c3f69b2c7b8d335
Veronika_Dikanova <dikanova.veronika@outlook.com>
2021-04-11 21:14:02 +0300

    Added 5 tasks about strings

--- Homework3/Homework3/Program.cs
+++ Homework3/Homework3/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace Homework3
 {
@@ -8,14 +9,19 @@ namespace Homework3
     {
         static void Main(string[] args)
         {
-            Task1(10);
-            //Task5();
-            //Task10();
-            //Task15();
-            //Task20();
+            //ArrayTask1(10);
+            //StringTask1();
+            //ArrayTask5();
+            //StringTask5();
+            //ArrayTask10();
+            //StringTask10();
+            //ArrayTask15();
+            //StringTask15();
+            //ArrayTask20();
+            StringTask20();
         }
 
-        public static void Task1(int dimension)
+        public static void ArrayTask1(int dimension)
         {
             Console.WriteLine($"Input {dimension} elements in the array: ");
             int[] array = Input(dimension);
@@ -23,7 +29,7 @@ namespace Homework3
             Output(array);
         }
 
-        public static void Task5()
+        public static void ArrayTask5()
         {
             Console.WriteLine("Input the number of elements to be stored in the array : ");
             int dimension = int.Parse(Console.ReadLine());
@@ -51,7 +57,7 @@ namespace Homework3
             Console.WriteLine($"Total number of duplicate elements found in the array is : {list.Count}");
         }
 
-        public static void Task10()
+        public static void ArrayTask10()
         {
             Console.Write("Input the number of elements to be stored in the array : ");
             int dimension = int.Parse(Console.ReadLine());
@@ -101,7 +107,7 @@ namespace Homework3
             Console.WriteLine("The Odds elements are: ");
             Output(odd);
         }
-        public static void Task15()
+        public static void ArrayTask15()
         {
             Console.Write("Input the size of array : ");
             int dimension = int.Parse(Console.ReadLine());
@@ -138,7 +144,7 @@ namespace Homework3
             Console.Write("The new list is : ");
             Output(resultarray);
         }
-        public static void Task20()
+        public static void ArrayTask20()
         {
             Console.Write("Input the size of the square matrix: ");
             int dimension = int.Parse(Console.ReadLine());
@@ -218,6 +224,105 @@ namespace Homework3
                 }
 
                 Console.WriteLine();
+            }
+        }
+        public static void StringTask1()
+        {
+            Console.WriteLine("Input the string : ");
+            string str = Console.ReadLine(); ;
+            Console.Write($"The string you entered is : {str}");
+        }
+        public static void StringTask5()
+        {
+            Console.WriteLine("Input the string : ");
+            string str = Console.ReadLine().Trim();
+            str = Regex.Replace(str, @"[^0-9a-zA-Z]", " ");
+            str = Regex.Replace(str, @"\s+", " ");
+            int wordsnumber = str.Split(' ').Length;
+            Console.WriteLine($"Total number of words in the string is : {wordsnumber}");
+        }
+        public static void StringTask10()
+        {
+            Console.WriteLine("Input the string : ");
+            string str = Console.ReadLine().Trim();
+            str = Regex.Replace(str, @"\s+", "");
+            char[] charlist = str.Distinct().ToArray();
+            int[] count = new int[charlist.Length];
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                for (int k = 0; k < charlist.Length; k++)
+                {
+                    if (str[i] == charlist[k])
+                    {
+                        count[k]++;
+                    }
+                } 
+            }
+
+            int index = Array.IndexOf(count, count.Max());
+            Console.WriteLine($"The Highest frequency of character '{charlist[index]}' appears number of times: {count.Max()} ");
+        }
+        public static void StringTask15()
+        {
+            Console.WriteLine("Input the string : ");
+            string str = Console.ReadLine().Trim();
+            char[] charlist =str.ToCharArray();
+
+            for (int i = 0; i < charlist.Length; i++)
+            {
+                if (char.IsUpper(charlist[i]))
+                {
+                    charlist[i] =char.ToLower(charlist[i]);
+                }
+                else
+                {
+                   charlist[i] = char.ToUpper(charlist[i]);
+                }
+            }
+
+            Console.Write("After conversion, the string is : ");
+
+            for (int i = 0; i < charlist.Length; i++)
+            {
+                Console.Write($"{charlist[i]}");
+            }
+        }
+        public static void StringTask20()
+        {
+            Console.WriteLine("Input the original string : ");
+            string str = Console.ReadLine().Trim();
+            Console.WriteLine("Input the string to be searched for : ");
+            string searchstr = Console.ReadLine().Trim();
+            Console.WriteLine("Input the string to be inserted : ");
+            string insertstr = Console.ReadLine().Trim();
+            str = Regex.Replace(str, @"\s+", " ");
+            string[] words = str.Split(' ');
+            string[] newstr = new string[words.Length + 1];
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (words[i] != searchstr)
+                {
+                    newstr[i] = words[i];
+                }
+                else
+                {
+                    newstr[i] = insertstr;
+
+                    for (int k = i + 1; k < words.Length + 1; k++)
+                    {
+                        newstr[k] = words[k - 1];
+                    }
+                    i = str.Length;
+                }
+            }
+
+            Console.WriteLine("The modified string is: ");
+
+            for (int i = 0; i < newstr.Length; i++)
+            {
+                Console.Write($"{newstr[i]} ");
             }
         }
     }
