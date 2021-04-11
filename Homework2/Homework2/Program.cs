3c7a887194b1032919f7de47c86a1d3330b67477
Veronika_Dikanova <dikanova.veronika@outlook.com>
2021-04-11 23:39:44 +0300

    Change file homework2, added new method

--- Homework2/Homework2/Program.cs
+++ Homework2/Homework2/Program.cs
@@ -11,96 +11,80 @@ namespace Homework2
             {
                 Console.WriteLine("Enter the day of week: ");
                 day = Console.ReadLine().ToLower();
-                int color;
-                int days = -1;
-                int numberofday = -1;
 
                 switch (day)
                 {
-                    case "mon":
-                    case "monday":
-                        day = "Monday";
-                        color = 1;
-                        numberofday = (int)DayOfWeek.Monday;
-                        days = 5;
+                    case "mon" or "monday":
+                        TasksWithDays(DayOfWeek.Monday);
                         break;
-                    case "tue":
-                    case "tuesday":
+                    case "tue" or "tuesday":
                         day = "Tuesday";
-                        color = 2;
-                        numberofday = (int)DayOfWeek.Tuesday;
-                        days = 4;
+                        TasksWithDays(DayOfWeek.Tuesday);
                         break;
-                    case "wed":
-                    case "wednesday":
+                    case "wed" or "wednesday":
                         day = "Wednesday";
-                        color = 3;
-                        numberofday = (int)DayOfWeek.Wednesday;
-                        days = 3;
+                        TasksWithDays(DayOfWeek.Wednesday);
                         break;
-                    case "thu":
-                    case "thursday":
+                    case "thu" or "thursday":
                         day = "Thursday";
-                        color = 4;
-                        numberofday = (int)DayOfWeek.Thursday;
-                        days = 2;
+                        TasksWithDays(DayOfWeek.Thursday);
                         break;
-                    case "fri":
-                    case "friday":
+                    case "fri" or "friday":
                         day = "Friday";
-                        color = 5;
-                        numberofday = (int)DayOfWeek.Friday;
-                        days = 1;
+                        TasksWithDays(DayOfWeek.Friday);
                         break;
-                    case "sat":
-                    case "suturday":
+                    case "sat" or "saturday":
                         day = "Saturday";
-                        color = 6;
-                        numberofday = (int)DayOfWeek.Saturday;
-                        days = 0;
+                        TasksWithDays(DayOfWeek.Saturday);
                         break;
-                    case "sun":
-                    case "sunday":
+                    case "sun" or "sunday":
                         day = "Sunday";
-                        color = 8;
-                        numberofday = (int)DayOfWeek.Sunday;
-                        days = 0;
+                        TasksWithDays(DayOfWeek.Sunday);
                         break;
                     case "exit":
                         day = "Exit the program";
-                        color = 15;
+                        Console.WriteLine($"{day}");
                         break;
                     default:
                         day = "There is no such day";
-                        color = 15;
+                        Console.WriteLine($"{day}");
                         break;
                 }
 
-                string today = DateTime.Today.DayOfWeek.ToString();
-                int numberdaytoday = (int)DateTime.Today.DayOfWeek;
-                DateTime nextday = DateTime.Today.AddDays((numberofday -numberdaytoday + 7) % 7);
-                if (nextday == DateTime.Today)
-                {
-                    nextday = nextday.AddDays(7);
-                }
-
-                Console.ForegroundColor = (ConsoleColor)color;
-                if (numberofday != -1)
-                {
-                    Console.WriteLine($"{day} - {numberofday}, {days} days before the weekend, {nextday} - next {day} date");
-                    if (today == day)
-                    {
-                        Console.WriteLine($"{today} is today");
-                    }
-                }
-                else
-                {
-                    Console.WriteLine($"{day}");
-                }
-                Console.ResetColor();
-
             }
 
+        }
+
+        public static void TasksWithDays(DayOfWeek day)
+        {
+            DayOfWeek today = DateTime.Today.DayOfWeek;
+            DateTime nextday = DateTime.Today.AddDays((day - today + 7) % 7);
+
+            if (nextday == DateTime.Today)
+            {
+                nextday = nextday.AddDays(7);
+            }
+
+            Console.ForegroundColor = (ConsoleColor)day + 1;
+            Console.WriteLine($"{day} - {(int)day}");
+            Console.WriteLine($"{nextday} - next {day} date");
+
+            if (today == day)
+            {
+                Console.WriteLine($"{today} is today");
+            }
+
+            if (day == DayOfWeek.Saturday || day == DayOfWeek.Sunday)
+            {
+                Console.WriteLine("0 days before the weekend");
+            }
+            else
+            {
+                
+                Console.WriteLine($"{DayOfWeek.Saturday - day} days before the weekend");
+            }
+
+            Console.ResetColor();
         }
     }
 }
