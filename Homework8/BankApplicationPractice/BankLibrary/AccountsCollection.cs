df299995d0ab98882489395cad143fec718f8cb8
Veronika_Dikanova <dikanova.veronika@outlook.com>
2021-04-28 10:58:06 +0300

    Added class AccountCollection: IEnumerable<Account>

---
+++ Homework8/BankApplicationPractice/BankLibrary/AccountsCollection.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace BankLibrary
+{
+    public class AccountsCollection : IEnumerable<Account>
+    {
+        private readonly List<Account> _accounts;
+
+        public AccountsCollection()
+        {
+            _accounts = new();
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            return _accounts.GetEnumerator();
+        }
+
+        public int GetCount()
+        {
+            return _accounts.Count; 
+        }
+
+        public void Add(Account account)
+        {
+            _accounts.Add(account);
+        }
+
+        public Account GetItem(int id)
+        {
+            return _accounts[id];
+        }
+
+        IEnumerator<Account> IEnumerable<Account>.GetEnumerator()
+        {
+            return _accounts.GetEnumerator();
+        }
+    }
+}
---
+++ Homework8/BankApplicationPractice/BankLibrary/AccountsCollectionEnumerator.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BankLibrary
+{
+    public class AccountsCollectionEnumerator : IEnumerator<Account>
+    {
+        private readonly List<Account> _accounts;
+        private int position = -1;
+
+        public AccountsCollectionEnumerator(List<Account> accounts)
+        {
+            _accounts = accounts;
+        }
+
+        public object Current
+        {
+            get
+            {
+                if (position == -1 || position >= _accounts.Count)
+                    throw new InvalidOperationException();
+                return _accounts[position];
+            }
+        }
+
+        public void Dispose()
+        {
+        }
+
+        public bool MoveNext()
+        {
+            if (position < _accounts.Count)
+            {
+                position++;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void Reset()
+        {
+            position = -1;
+        }
+
+        Account IEnumerator<Account>.Current
+        {
+            get
+            {
+                if (position == -1 || position >= _accounts.Count)
+                    throw new InvalidOperationException();
+                return _accounts[position];
+            }
+        }
+    }
+}
