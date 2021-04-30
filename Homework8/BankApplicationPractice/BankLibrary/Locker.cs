db5e6e1cba4f1e39d2fa36887a07ee10b020a046
Veronika_Dikanova <dikanova.veronika@outlook.com>
2021-04-30 17:24:59 +0300

    Added logic for adding lockers,replace List<locker> on Dictionary<Locker,object>, fixed bugs in AccountCollection

---
+++ Homework8/BankApplicationPractice/BankLibrary/Locker.cs
@@ -0,0 +1,36 @@
+﻿namespace BankLibrary
+{
+    public class Locker
+    {
+        private string _keyword;
+        private int _id;
+
+        public Locker(int id, string keyword)
+        {
+            _id = id;
+            _keyword = keyword;
+        }
+
+        public int Id => _id;
+
+        public bool Matches(int id, string keyword)
+        {
+            return _id == id && _keyword.Equals(keyword);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is not Locker locker)
+            {
+                return false;
+            }
+
+            return Id == locker.Id && _keyword.Equals(locker._keyword);
+        }
+
+        public override int GetHashCode()
+        {
+            return Id ^ _keyword.GetHashCode();
+        }
+    }
+}
